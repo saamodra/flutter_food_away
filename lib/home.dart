0cc55f08937367eb6589a515e7a9533eb5da6fea
Samodra <38998978+saamodra@users.noreply.github.com>
2021-08-23 23:56:19 +0700

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -268,6 +268,56 @@ class Home extends StatelessWidget {
                         ],
                       )
                     ),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: FaColor.secondary, width: 0.1)
+                      ),
+                      padding: const EdgeInsets.all(25.0),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Image.asset("images/fa-logo.png", width: 50, height: 50,),
+                              Container(
+                                margin: EdgeInsets.only(left: 20),
+                                child: Text("Food Away", style: TextStyle(
+                                  fontSize: 26,
+                                  fontFamily: 'OpenSansBold',
+                                  color: FaColor.orangePrimary
+                                )),
+                              )
+                            ],
+                          ),
+                          Container(
+                            margin: EdgeInsets.symmetric(vertical: 30),
+                            child: Text("Dapatkan info mengenai restoran terbaik di sekitarmu hanya di Food Away.",
+                              style: TextStyle(
+                                fontSize: 18,
+                                height: 1.5,
+                                fontFamily: 'OpenSans',
+                                color: FaColor.secondary
+                              ),
+                            )
+                          ),
+                          Container(
+                            padding: const EdgeInsets.only(top: 20),
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              border: Border(
+                                top: BorderSide(width: 0.2, color: FaColor.secondary)
+                              )
+                            ),
+                            child: Text("2021 © Samodra", textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontFamily: 'OpenSans',
+                                color: Color(0xff737373)
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
                   ],
                 ),
               ),
